0f7f6d0620d8141e4674dfe18bbee266851f6447
kaiTanobe <stomp0630@icloud.com>
2021-05-21 13:41:43 +0900

    Initial Commit

---
+++ practice_for_C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practice_for_C++
+//
+//  Created by 田野辺開 on 2021/05/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
